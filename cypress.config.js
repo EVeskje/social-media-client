aa4b367e8bd3630ff5055c3b1769933053db9a5a
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2024-04-24 23:20:04 +0200

    Edit cypress testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    validEmail: "worktester@noroff.no",
+    validPassword: "passord123",
+    wrongEmail: "wrong@email.com",
+    wrongPassword: "wrongPassword",
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
